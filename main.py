0ba4232fbd3548076dbf04cc91ac0175fd698efd
Calvin <cdy3870@gmail.com>
2020-02-20 13:27:30 -0500

    upload page

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def rent():
 	return render_template('rent.html')
 
+@app.route('/upload')
+def upload():
+	return render_template('upload.html')
+
 if __name__ == '__main__':
 	app.run(debug=True)
