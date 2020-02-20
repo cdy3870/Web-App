d005428427855fdf9b2ca1bd5ffa421bfa3c2efe
Calvin Yu <thefallen3870@yahoo.com>
2020-02-20 12:53:27 -0500

    main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/welcome')
 def welcome():
-	return render_template('welcome.html')
+	return render_template('rent.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
