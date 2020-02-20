bf4f8c16859c2daa7500a12b4f99aee03e59417e
Calvin <cdy3870@gmail.com>
2020-02-20 12:13:51 -0500

    main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+
+@app.route('/welcome')
+def welcome():
+	return render_template('welcome.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)
